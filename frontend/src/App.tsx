14fcf330501b57d3bc36d1e8559f6affedeb2cd7
pleum-attakorn <Attakorm.su@ku.th>
2022-03-26 14:51:52 +0700

    init frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cell from './components/Cell/Cell'
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Cell>Hello</Cell>
   );
 }
 
