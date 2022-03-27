d887bfcdcda5ea7e5f1b66aaed2aaf5a42981e12
pleum-attakorn <Attakorm.su@ku.th>
2022-03-27 12:35:06 +0700

    add resizer

--- frontend/src/components/Resizer/Resizer.tsx
+++ frontend/src/components/Resizer/Resizer.tsx
@@ -1,10 +1,35 @@
 import React, {FunctionComponent} from "react";
+import { useResetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
+import { SheetSizeState } from "../../store/SheetSizeState";
 import classes from "./Resizer.module.css";
 
 export type ResizerProps = {};
 
 const Resizer: FunctionComponent<ResizerProps> = (props) => {
-    return <div className={classes.Resizer} />;
+    const [sheetSize, setSheetSize] = useRecoilState(SheetSizeState);
+
+    const initDrag = () => {
+        document.addEventListener('mousemove', doDrag);
+        document.addEventListener('mouseup', stopDrag);        
+    }
+
+    const doDrag = (event: MouseEvent) => {
+        const pointerX = event.pageX;
+        const pointerY = event.pageY;
+
+        setSheetSize({
+            width: pointerX,
+            height: pointerY,
+        });
+    };
+
+    const stopDrag = () => {
+        document.removeEventListener('mousemove', doDrag);
+        document.removeEventListener('mouseup', stopDrag);
+    };
+
+    return <div onMouseDown={initDrag} className={classes.Resizer} />;
 };
 
 export default Resizer;
