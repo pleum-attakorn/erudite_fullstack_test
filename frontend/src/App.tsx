33edb85e0b19bc10a2a3b845be0a650772ba40e2
pleum-attakorn <Attakorm.su@ku.th>
2022-03-29 22:54:45 +0700

    Addrow function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { RecoilRoot } from 'recoil';
 import './App.css';
 import SheetsContainer from './containers/SheetsContainer'
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState('test message');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/weather/')
+      .then(res => res.json())
+      .then(obj => {
+        setMessage(obj.message);
+      });
+  },[]);
+
   return (
     <RecoilRoot>
       <SheetsContainer />
-    </RecoilRoot>    
+      <div>{message}</div>
+    </RecoilRoot>      
   );
 }
 
